ed881c2a0cfa20aa78a80ad6a74ca184b316337c
Brown Memoye <memoyebrown@gmail.com>
2025-01-16 14:56:01 +0100

    get actual customer invoice

--- src/app/api/invoice/[invoiceId]/route.tsx
+++ src/app/api/invoice/[invoiceId]/route.tsx
@@ -1,39 +1,67 @@
-import QRCode from "qrcode";
 import { NextRequest } from "next/server";
+import { renderToBuffer } from "@react-pdf/renderer";
+import api from "@/lib/api";
 import { InvoicePDF } from "@/components/InvoicePDF";
 import { registerFonts } from "@/lib/pdfStyles";
-import { renderToBuffer } from "@react-pdf/renderer";
 import { generateQRWithLogo } from "@/lib/utils";
+import { Invoice } from "@/lib/types";
 
 export const GET = async (
   request: NextRequest,
   { params }: { params: Promise<{ invoiceId: string }> }
 ) => {
   registerFonts();
-
-  const invoiceId = (await params).invoiceId;
   const searchParams = request.nextUrl.searchParams;
 
-  const urlStr = "https://google.com";
+  const invoiceId = (await params).invoiceId;
+  const token = searchParams.get("token");
 
-  const qrCodeDataUrl = await generateQRWithLogo(urlStr);
+  const res = await api.get(`/invoice/customer/${invoiceId}`, {
+    params: {
+      token: token || "z2KTbouczuVwLkIAMXwqYXwABWCFx2gTsFeczzbb6yc",
+      otp: "123456",
+    },
+  });
 
-  // QRCode.toDataURL(urlStr, {
-  //   errorCorrectionLevel: "M",
-  //   margin: 4,
-  //   width: 150,
+  if (res.status === 404) {
+    return Response.json(
+      { message: "Invoice does not exist" },
+      { status: 404 }
+    );
+  }
 
-  //   scale: 4,
-  // });
+  if (res.status !== 200) {
+    return Response.json(
+      { message: res.data.message || "Something went wrong" },
+      { status: 500 }
+    );
+  }
+
+  const invoiceDetails = res.data?.data as Invoice;
+
+  const qrCodeDataUrl = await generateQRWithLogo(invoiceDetails.link);
+
+  const selectedCustomerIndex =
+    Math.max(
+      0,
+      invoiceDetails?.customers?.findIndex(
+        (invoice) =>
+          invoice.customerInvoiceNumber === invoiceDetails.invoiceNumber
+      )
+    ) ?? 0;
 
   const pdf = await renderToBuffer(
-    <InvoicePDF qrCode={qrCodeDataUrl} selectedCustomerIndex={0} />
+    <InvoicePDF
+      qrCode={qrCodeDataUrl}
+      selectedCustomerIndex={selectedCustomerIndex}
+      invoiceDetails={invoiceDetails}
+    />
   );
 
   return new Response(pdf, {
     headers: {
       "Content-Type": "application/pdf",
-      // "Content-disposition": `attachment;filename="myDocument.pdf"`, // to
+      // "Content-disposition": `attachment;filename="myDocument.pdf"`, // this triggers download
     },
     status: 200,
   });
